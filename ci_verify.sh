03bf005be233f7164cedb2d86ece25ddb2a292c7
Giles Knap <giles.knap@diamond.ac.uk>
2024-01-30 14:21:04 +0000

    update to latest charts

--- ci_verify.sh
+++ ci_verify.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# CI to verify all the IOC instances specified in this repo
+# CI to verify all the instances specified in this repo have valid configs
 # Domain agnostic - this script is generic for all beamlines/domains
 
 THIS=$(dirname ${0})
 set -e
 
-for app in ${THIS}/apps/*
+for app in ${THIS}/services/*
 do
     # Skip if subfolder has no config to validate
     if [ ! -d "${ioc}/config" ]; then
