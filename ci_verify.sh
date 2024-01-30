591c546254f9b2fdac5bcc173eeae723dc1d1028
Giles Knap <giles.knap@diamond.ac.uk>
2024-01-30 09:02:36 +0000

    second try at restructure

--- ci_verify.sh
+++ ci_verify.sh
@@ -3,10 +3,10 @@
 # CI to verify all the IOC instances specified in this repo
 # Domain agnostic - this script is generic for all beamlines/domains
 
-THIS_DIR=$(dirname ${0})
+THIS=$(dirname ${0})
 set -e
 
-for ioc in ${THIS_DIR}/iocs/*
+for app in ${THIS}/apps/*
 do
     # Skip if subfolder has no config to validate
     if [ ! -d "${ioc}/config" ]; then
